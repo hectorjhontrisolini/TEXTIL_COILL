6a304699ccb32ff70fc9251c68fe260d2c76a6d7
hectorjhontrisolini <jhontrisolini@gmail.com>
2026-09-12 19:11:55 -0500

    Update base path in vite.config.ts
    Changed the base path from '/TEXTIL_COILL/' to '/' in the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/TEXTIL_COILL/',  // ← ESTA LÍNEA es la importante
+  base: '/',  // ← ESTA LÍNEA es la importante
   plugins: [react()],
   resolve: {
     alias: {
